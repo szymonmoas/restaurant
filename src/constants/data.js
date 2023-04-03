ad65e67b81ad0a226868d6cf617e09a8e34c1b41
unknown <szymonmoas@gmail.com>
2023-04-03 12:35:11 +0200

    first commit

---
+++ src/constants/data.js
@@ -0,0 +1,82 @@
+import images from './images';
+
+const wines = [
+  {
+    title: 'Lorem ipsum Dolor',
+    price: '$56',
+    tags: 'Lorem | Lorem ipsum',
+  },
+  {
+    title: 'Lorem Dolor',
+    price: '$59',
+    tags: 'Lorem | Lorem ipsum',
+  },
+  {
+    title: 'Lorem ipsum Dolor',
+    price: '$44',
+    tags: 'Lorem | Lorem',
+  },
+  {
+    title: 'Lorem ipsum',
+    price: '$31',
+    tags: 'Lorem ipsum |Lorem ipsum',
+  },
+  {
+    title: 'Lorem ipsum Dolor',
+    price: '$26',
+    tags: 'Lorem | Lorem',
+  },
+];
+
+const cocktails = [
+  {
+    title: 'Ipsum Dolor',
+    price: '$20',
+    tags: 'Lorem | Lorem ipsum | Lorem | Lorem',
+  },
+  {
+    title: "Lorem ipsum Dolor",
+    price: '$16',
+    tags: 'Lorem ipsum | Lorem | Lorem ipsum',
+  },
+  {
+    title: 'Lorem ipsum',
+    price: '$10',
+    tags: 'Lorem | Lorem ipsum | Lorem ipsum',
+  },
+  {
+    title: 'Lorem Dolor',
+    price: '$31',
+    tags: 'Lorem | Lorem ipsum Dolor | Lorem ipsum',
+  },
+  {
+    title: 'Lorem',
+    price: '$26',
+    tags: 'Lorem | Lorem ipsum | Lorem | Lorem ipsum',
+  },
+];
+
+const awards = [
+  {
+    imgUrl: images.award02,
+    title: 'Lorem ipsum',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award01,
+    title: 'Lorem ipsum',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award05,
+    title: 'Lorem ipsum Dolor',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award03,
+    title: 'Lorem ipsum',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+];
+
+export default { wines, cocktails, awards };
---
+++ src/constants/images.js
@@ -0,0 +1,31 @@
+import bg from '../assets/bg.jpg';
+import chef from '../assets/chef.jpg';
+import gallery01 from '../assets/gallery01.jpg';
+import gallery02 from '../assets/gallery02.jpg';
+import gallery03 from '../assets/gallery03.jpg';
+import gallery04 from '../assets/gallery04.jpg';
+import knife from '../assets/knife.jpg';
+import menu from '../assets/menu.jpg';
+import spoon from '../assets/spoon.svg';
+import welcome from '../assets/welcome.jpg';
+import findus from '../assets/findus.jpg';
+import laurels from '../assets/laurels.jpg';
+import quote from '../assets/quote.png';
+import restaurant from '../assets/restaurant.png';
+
+export default {
+  bg,
+  chef,
+  gallery01,
+  gallery02,
+  gallery03,
+  gallery04,
+  knife,
+  menu,
+  spoon,
+  welcome,
+  findus,
+  laurels,
+  quote,
+  restaurant,
+};
